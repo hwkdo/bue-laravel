9af59746aff9aeb174152455b3dd3f01f2865533
aleex1848 <halycon01@gmail.com>
2025-07-28 14:11:37 +0200

    initial release

--- config/bue-laravel.php
+++ config/bue-laravel.php
@@ -2,5 +2,7 @@
 
 // config for Hwkdo/BueLaravel
 return [
-
+    'database' => [
+        'connection' => env('BUEDB_CONNECTION', 'universal-utf8'),
+    ],
 ];
--- src/BueLaravel.php
+++ src/BueLaravel.php
@@ -2,4 +2,38 @@
 
 namespace Hwkdo\BueLaravel;
 
-class BueLaravel {}
+use Illuminate\Support\Facades\DB;
+
+class BueLaravel {
+
+    public function getFachbereiche() {
+        return DB::connection(config('bue-laravel.database.connection'))
+        ->table('intranet.v_fachbereiche')
+        ->select('*')
+        ->get();        
+    }
+
+    public function getGewerke()
+    {
+        return DB::connection(config('bue-laravel.database.connection'))
+        ->table('intranet.v_gewerbe')
+        ->select('*')
+        ->get();        
+    }
+
+    public function getEintragungsvorraussetzungen()
+    {
+        return DB::connection(config('bue-laravel.database.connection'))
+        ->table('intranet.v_eintragungsvoraussetzung')
+        ->select('*')
+        ->get();  
+    }
+
+    public function getRechtsformen()
+    {
+        return DB::connection(config('bue-laravel.database.connection'))
+        ->table('intranet.v_rechtsform')
+        ->select('*')
+        ->get();  
+    }
+}
